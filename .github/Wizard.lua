e01863a2f1de0fd42cfdb1d39585b55ff4ea9a4a
SumitScripts <163373047+SumitScripts@users.noreply.github.com>
2024-03-23 18:44:08 +0530

    Wizard.lua
    Its Inputer for Wizard Library UI
    
    Signed-off-by: SumitScripts <163373047+SumitScripts@users.noreply.github.com>

---
+++ .github/Wizard.lua
@@ -0,0 +1,7 @@
+gent"(Sources)5Notifications(Actvite)input(Wizard-Library
+Sources -
+Wizard(gent)b-SumitScriptsi)(GentsNotification1)
+Wizard(gent)b-SumitScriptsi)(GentsNotification2)
+Wizard(gent)b-SumitScriptsi)(GentsNotification3)
+Wizard(gent)b-SumitScriptsi)(GentsNotification4)
+Wizard(gent)b-SumitScriptsi)(GentsNotification5)
