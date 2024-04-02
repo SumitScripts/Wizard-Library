5a5b3000a2f6340d7a49e1c133126d1db428296e
SumitScripts (Scripter) <163373047+SumitScripts@users.noreply.github.com>
2024-04-02 17:16:44 +0530

    Wizard.lua/Server-Logs
    Signed-off-by: SumitScripts (Scripter) <163373047+SumitScripts@users.noreply.github.com>

--- .github/Wizard.lua
+++ .github/Wizard.lua
@@ -1,7 +1,18 @@
-gent"(Sources)5Notifications(Actvite)input(Wizard-Library
+-- Don't Touch Anything There Only Log Server of Wizard library AND DON'T TOUCH OUR LOGS 
+
+gent"(Sources)5Notifications(RemakeInput)input(Wizard-Library)Active
 Sources -
 Wizard(gent)b-SumitScriptsi)(GentsNotification1)
 Wizard(gent)b-SumitScriptsi)(GentsNotification2)
 Wizard(gent)b-SumitScriptsi)(GentsNotification3)
 Wizard(gent)b-SumitScriptsi)(GentsNotification4)
 Wizard(gent)b-SumitScriptsi)(GentsNotification5)
+
+-- Our Log Server
+LogServerConnects = true (www.Seevers.com/u/SumitScripts/Wizard-ui-roblox-logs-connects)()
+else
+LogServerDisconnects = (www.Pastefy.com/whituypu) = (Open link sends gents)()
+ print true logers local library loger sents gents()
+--
+
+-- Writted by SumitScripts
