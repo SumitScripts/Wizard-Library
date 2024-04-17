27bfbbc75c182e37a0853ef91ae70d5af694ecd5
SumitScripts (Scripter) <163373047+SumitScripts@users.noreply.github.com>
2024-04-17 18:46:32 +0530

    Update Wizard.lua
    Signed-off-by: SumitScripts (Scripter) <163373047+SumitScripts@users.noreply.github.com>

--- .github/Wizard.lua
+++ .github/Wizard.lua
@@ -8,11 +8,16 @@ Wizard(gent)b-SumitScriptsi)(GentsNotification3)
 Wizard(gent)b-SumitScriptsi)(GentsNotification4)
 Wizard(gent)b-SumitScriptsi)(GentsNotification5)
 
+-- New Update Source
+Wizard(gent)b-SumitScriptsi)(GentsNotification6)
+Wizard(gent)b-SumitScriptsi)(GentsNotification7)
+Wizard(gent)b-SumitScriptsi)(GentsNotification8)
+Wizard(gent)b-SumitScriptsi)(GentsNotification9)
+Wizard(gent)b-SumitScriptsi)(GentsNotification10)
+
 -- Our Log Server
 LogServerConnects = true (www.Seevers.com/u/SumitScripts/Wizard-ui-roblox-logs-connects)()
 else
-LogServerDisconnects = (www.Pastefy.com/whituypu) = (Open link sends gents)()
+LogServerDisconnects = (www.Pastefy.com/raw/whitu74ypu5) = (Open link sends gents)()
  print true logers local library loger sents gents()
---
 
--- Writted by SumitScripts
